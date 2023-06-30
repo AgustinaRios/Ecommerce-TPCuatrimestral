962dcda07a1a731b5a079d3d56da7432a398c33a
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2023-06-30 20:27:47 -0300

    fondo maximo en pantallas

--- TiendaVinilos/TiendaVinilos/AlbumsxGenero.aspx.cs
+++ TiendaVinilos/TiendaVinilos/AlbumsxGenero.aspx.cs
@@ -16,11 +16,12 @@ namespace TiendaVinilos
                 if (Request.QueryString["idgenero"]!=null)
                 {
                     Int32 IdGenero = Int32.Parse(Request.QueryString["idgenero"]);
-                    Session.Add("idgenero", IdGenero);
+                    Session.Add("id", IdGenero);//se la necesita por que se pierde el id del gnero cuando se recrga la pg,por ejemplo cuando haces clic en btncarrito
                     listaAlbum = negocio.listarxGenero(IdGenero);
                     Session.Add("Listaalbum", listaAlbum);
                 }
                 else { 
+                    //si idgnero es nulo quiere decir que se cargo la pag por hacer click en btncarrito
                 Int32 IdGenero2 = (Int32)Session["idgenero"] ;
                 listaAlbum = negocio.listarxGenero(IdGenero2);
                 Session.Add("Listaalbum", listaAlbum);
