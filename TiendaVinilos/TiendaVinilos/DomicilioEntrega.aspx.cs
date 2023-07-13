b1f9bf09e2944d9f644c8f51850327adde4a722d
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2023-07-13 13:14:37 -0300

    validacion domicilioentrega.aspx

--- TiendaVinilos/TiendaVinilos/DomicilioEntrega.aspx.cs
+++ TiendaVinilos/TiendaVinilos/DomicilioEntrega.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Drawing;
 using Dominio;
 namespace TiendaVinilos
 {
@@ -14,10 +15,38 @@ namespace TiendaVinilos
 
         }
 
+        bool ValidarVacios()
+        {
+
+           
+            TxtDireccion.BorderColor = Color.White;
+            TxtLocalidad.BorderColor = Color.White;
+            TxtProvincia.BorderColor = Color.White;
+
+            bool vacios = false;
+            
+            if (TxtDireccion.Text == "")
+            {
+                TxtDireccion.BorderColor = Color.Red;
+                vacios = true;
+            }
+            if (TxtLocalidad.Text == "")
+            {
+                TxtLocalidad.BorderColor = Color.Red;
+                vacios = true;
+            }
+            if (TxtProvincia.Text == "")
+            {
+                TxtProvincia.BorderColor = Color.Red;
+                vacios = true;
+            }
+
+            return vacios;
+        }
         protected void BtnAceptar_Click(object sender, EventArgs e)
         {
             Pedido domicilio = new Pedido();
-            if (TxtDireccion.Text == "" || TxtLocalidad.Text == "" || TxtProvincia.Text == "")
+            if (ValidarVacios()==true)
             {
                 LblMensaje.Text = "Complete todos los campos";
                 LblMensaje.Visible = true;
